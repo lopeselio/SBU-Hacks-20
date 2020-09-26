0b7c3c31614830c47e2afb8203d2170c4ca1d616
lopeselio <lopeselio@gmail.com>
2020-09-27 00:09:27 +0530

    added progress

---
+++ EnemyAttack.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    // [SerializeField] Transform target;
+    PlayerHealth target;
+    [SerializeField] float damage = 40f;
+
+    void Start()
+    {
+
+        target = FindObjectOfType<PlayerHealth>();
+    }
+
+    
+    public void OnDamageTaken()
+    {
+        Debug.Log(name + "I know I took damage");
+    }
+
+    public void AttackHitEvent()
+    {
+        if (target == null) return;
+        target.TakeDamage(damage);
+        Debug.Log("bang bang");
+    }
+
+}
